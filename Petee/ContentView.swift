2d3340a532cbff050d3f84f7c266b3e38ccf76c1
MZIO <mziodev@gmail.com>
2024-05-20 09:40:27 +0200

    Initial Commit

---
+++ Petee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Petee
+//
+//  Created by Mauricio dSR on 20/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Petee/PeteeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeteeApp.swift
+//  Petee
+//
+//  Created by Mauricio dSR on 20/5/24.
+//
+
+import SwiftUI
+
+@main
+struct PeteeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
